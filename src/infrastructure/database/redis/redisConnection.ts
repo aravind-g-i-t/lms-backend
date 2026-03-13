267c00c3002ea727cc1a59a1cdbacde6e5501f64
Aravind <aravindunni2011999@gmail.com>
2026-03-13 10:16:53 +0530

    update redis-connection

--- src/infrastructure/database/redis/redisConnection.ts
+++ src/infrastructure/database/redis/redisConnection.ts
@@ -4,27 +4,30 @@ import { createClient, RedisClientType } from "redis";
 let redisClient: RedisClientType;
 
 export async function connectRedis(): Promise<void> {
-    try {
-        redisClient = createClient({
-            url: process.env.REDIS_URL,
-        }) as RedisClientType;
+  try {
+    redisClient = createClient({
+      url: process.env.REDIS_URL,
+      socket: {
+        tls: true
+      }
+    }) as RedisClientType;
 
-        redisClient.on("connect", () => {
-            logger.info("Connected to Redis");
-        });
+    redisClient.on("connect", () => {
+      logger.info("Connected to Redis");
+    });
 
-        redisClient.on("error", () => {
-            logger.error("Redis error");
-        });
+    redisClient.on("error", (err) => {
+      logger.error(`Redis error: ${err}`);
+    });
 
-        await redisClient.connect();
-    } catch {
-        logger.error("Failed to connect to Redis.");
-        return process.exit(1);
-    }
+    await redisClient.connect();
+  } catch {
+    logger.error("Failed to connect to Redis.");
+    process.exit(1);
+  }
 }
 
 export function getRedisClient(): RedisClientType {
-    if (!redisClient) throw new Error("Redis not connected");
-    return redisClient;
+  if (!redisClient) throw new Error("Redis not connected");
+  return redisClient;
 }
